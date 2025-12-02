2ffea2489ada059b42c03bad2e6d14ad41078484
asthap1112-lgtm <asthap1112@gmail.com>
2025-12-02 12:14:32 +0530

    Delete model.js

--- model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const bookSchema = new mongoose.Schema({
-    title: {type:String, required: true},
-    author:{type:String, required: true},
-    year:{type:Number},
-    genre:{type:String}
-});
-moule.exports = mongoose.model('book',bookSchema);
